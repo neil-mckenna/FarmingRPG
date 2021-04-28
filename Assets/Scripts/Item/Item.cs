02c70d54a6071ee0464790a2931e955703d93524
TUFFY <emberhawk97@gmail.com>
2021-04-28 15:54:37 +0100

    Created an item prefab and Item class, created preab variant and made 3 dffiernt commodities

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private int _itemCode;
+    
+    private SpriteRenderer spriteRenderer;
+
+    public int ItemCode { get { return _itemCode; } set { _itemCode = value; } }
+
+    private void Awake() 
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        
+    }
+
+    private void Start() 
+    {
+        if( ItemCode != 0 )
+        {
+            Init(ItemCode);
+        }
+
+
+
+
+        
+    }
+
+    public void Init(int itemCodeParam)
+    {
+
+    }
+
+
+}
