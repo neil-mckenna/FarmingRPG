5bb9f1ba4a1ee44fa1b85f618609a9a48d220e10
TUFFY <emberhawk97@gmail.com>
2021-04-28 14:53:48 +0100

    Created Item Details class and change enum for itemtype

--- Assets/Scripts/Enums/Enums.cs
+++ Assets/Scripts/Enums/Enums.cs
@@ -14,3 +14,19 @@ public enum Direction
     none,
 
 }
+
+public enum ItemType
+{
+    Seed,
+    Commodity,
+    Watering_tool,
+    Hoeing_tool,
+    Chopping_tool,
+    Breaking_tool,
+    Reaping_tool,
+    Collecting_tool,
+    Reapable_scenery,
+    none,
+    count,
+
+}
