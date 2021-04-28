6ad1a291196043332864060931b69a3eaac53dc3
TUFFY <emberhawk97@gmail.com>
2021-04-28 13:42:27 +0100

    Added new scenery and made prefabs from them, created a camera bounds feature to the virtual camera

---
+++ Assets/Scripts/Scene/SwitchConfineBoundingShape.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class SwitchConfineBoundingShape : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchBoundingShape();
+        
+    }
+
+    /// <summary>
+    ///   Switch the collider that cinemachine uses to define teh edges of the screen
+    ///</summary>
+
+
+
+    private void SwitchBoundingShape()
+    {
+        // Get the polygon collider on the 'boundsconfiner' gameobject which is used by cinemachine to prevent teh camera going beyong the screen edges
+        PolygonCollider2D polygonCollider2D = GameObject.FindGameObjectWithTag(Tags.BoundsConfiner).GetComponent<PolygonCollider2D>();
+
+        CinemachineConfiner cinemachineConfiner = GetComponent<CinemachineConfiner>();
+
+        cinemachineConfiner.m_BoundingShape2D = polygonCollider2D;
+
+        // since teh confiner bounds have changed need to call this to clear the cache.
+        cinemachineConfiner.InvalidatePathCache();
+    }
+
+
+}
